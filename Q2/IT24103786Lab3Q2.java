5351c195611bb85dfa89ac275ad6c24f140f8a90
IT24103786 <It24103786@my.sliit.lk>
2026-09-23 07:59:12 +0530

    Add files via upload

---
+++ Q2/IT24103786Lab3Q2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class IT24103786Lab3Q2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the monthly salary: ");
+        double monthlySalary = input.nextDouble();
+
+        System.out.print("Enter the number of OT hours: ");
+        double otHours = input.nextDouble();
+
+        System.out.print("Enter the OT hourly rate: ");
+        double otRate = input.nextDouble();
+
+        double otAmount = otHours * otRate;
+        double totalSalary = monthlySalary + otAmount;
+
+        System.out.println();
+        System.out.println("The total salary including OT is: " + totalSalary);
+    }
+}
